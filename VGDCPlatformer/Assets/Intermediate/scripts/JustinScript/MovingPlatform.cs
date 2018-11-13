83f93df9d3c82785b36c4172552f3f02295bbb61
JC8299 <justincheng.cheng@gmail.com>
2018-11-13 15:16:43 -0800

    adding my scripts to the group projects

---
+++ VGDCPlatformer/Assets/Intermediate/scripts/JustinScript/MovingPlatform.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    //manages platform movement at different points
+    [Header("Platform Positions")]
+    public Transform movingPlatform; //platform gameObject that we will be moving
+
+    /*Points where the platform  will move*/
+    public Transform position1; 
+    public Transform position2;
+    public Transform position3;
+
+    [HideInInspector] public Vector3 newPosition; //(X, Y, Z) coordinates of the platform
+
+    [Space]
+
+    [Header("Platform Attributes")]
+    public string state; //named states on where the platform should move
+    
+    /*Adjusted speeds for the platform movement */
+    public float platformVelocity;
+    public float movementTime;
+
+    void Start()
+    {
+        //initally the platform will start at some state
+        ChangeTarget();
+    }
+
+    void FixedUpdate()
+    {
+        //update the movement of the platform
+        movingPlatform.position = Vector3.Lerp(movingPlatform.position, newPosition, platformVelocity * Time.deltaTime);
+    }
+
+    void ChangeTarget()
+    {
+        /*check states and move platform, then we change states to update
+        postion of the moving platform */
+
+        //state names are abritrary and can program more 
+        //states and positions if needed
+        if (state == "right1")
+        {
+            state = "right2";
+            newPosition = position2.position;
+            movementTime = 1;
+            Debug.Log("right1");
+        }
+        else if (state == "right2")
+        {
+            state = "left1";
+            newPosition = position3.position;
+            movementTime = 2;
+            Debug.Log("right2");
+        }
+        else if (state == "left1")
+        {
+            state = "left2";
+            newPosition = position2.position;
+            movementTime = 1;
+            Debug.Log("left1");
+        }
+        else if (state == "left2")
+        {
+            state = "right1";
+            newPosition = position1.position;
+            movementTime = 2;
+            Debug.Log("left2");
+        }
+        else if (state == "start")
+        {
+            state = "right2";
+            newPosition = position2.position;
+            movementTime = 1;
+            Debug.Log("start");
+        }
+        else if (state == "stop") //default position
+        {
+            newPosition = position1.position;
+            movementTime = 0;
+            if (Input.GetButton("Fire1"))
+            {
+                state = "right1";
+                Debug.Log("Fire1");
+            }
+        }
+        Invoke("ChangeTarget", movementTime);
+    }
+
+}
---
+++ VGDCPlatformer/Assets/Intermediate/scripts/JustinScript/Platform.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    public string stateplatform;
+    public float disappeartime;
+    int count = 0;
+    SpriteRenderer platformspriterenderer;
+    BoxCollider2D platformcollider;
+
+    void Start()
+    {
+        platformspriterenderer = GetComponent<SpriteRenderer>();
+        platformcollider = GetComponent<BoxCollider2D>();
+    }
+    
+   
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        //set the player as a child to the platform gaemObject
+        //this allows movement of the player with moving platforms
+        if (collision.gameObject.CompareTag("Player"))
+        { 
+
+            collision.collider.transform.SetParent(transform);
+        }
+        
+        if ((stateplatform == "delete" || stateplatform == "transparent") && !IsInvoking("disappearing"))
+        {
+            InvokeRepeating("disappearing", 1, 1);
+            if (stateplatform == "delete")
+            {
+                StartCoroutine("buhbye", collision);
+            }
+            else if (stateplatform == "transparent")
+            {
+                StartCoroutine("goobye", collision);
+            }
+            //animate the platform to shake
+            //decides whether to delete platform or let the platform return after a while
+            
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        //removes the player from being a child of this platform gameObject
+        //once player is not on the platform.
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.collider.transform.SetParent(null);
+        }
+    }
+
+    //deletes the platfrom and moves the player from being a child of the platform
+    IEnumerator buhbye(Collision2D collision)
+    {
+        //makes the whole coroutine wait for indicated seconds
+        yield return new WaitForSeconds(disappeartime);
+        //sets player's parent to null so it doesn't get deleted with platform
+        collision.collider.transform.SetParent(null);
+        //deletes the platform 1 second after
+        Destroy(gameObject, 1f);
+    }
+    //makes the platform return after a while
+    IEnumerator goobye(Collision2D collision)
+    {
+        //make temp alpha to keep colors and change the alpha
+        Color c = platformspriterenderer.color;
+        //alpha value
+        c.a = .3f;
+        //makes the whole coroutine wait for indicated seconds
+        yield return new WaitForSeconds(disappeartime);
+        //sets player's parent to null so platform doesn't affect the player too
+        collision.collider.transform.SetParent(null);
+        //changes platform alpha then disables the platform collider
+        platformspriterenderer.color = c;
+        //makes the platform uncollidable
+        platformcollider.enabled = !platformcollider.enabled;
+        //waits for respawn
+        yield return new WaitForSeconds(disappeartime);
+        //changes temp alpha to completely visible, then changes the platform alpha and reenables collider
+        c.a = 1f;
+        platformspriterenderer.color = c;
+        platformcollider.enabled = !platformcollider.enabled;
+    }
+
+    void disappearing()
+    {
+        //counter to prevent multiple activations of the platforms
+        Debug.Log(count);
+        if (count < 10)
+        {
+            count++;
+        }
+        if (count == 10)
+        {
+            count = 0;
+            Debug.Log("done");
+            CancelInvoke();
+        }
+    }
+}
+//animate platform
+
+//moving platform triggered from anywhere
