1d738b0c740b5c1a5ec9b91dc5e1eb343c43bd6f
John Wang <johnw7@uci.edu>
2018-11-13 19:55:21 -0800

    John Update - Added Coments
    Added comments on how to use scripts
    Update TheDoor.cs to let people choose what scene they want to load

--- VGDCPlatformer/Assets/Beginner/Scripts/OutOfBound.cs
+++ VGDCPlatformer/Assets/Beginner/Scripts/OutOfBound.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OutOfBound : MonoBehaviour {
-    [SerializeField] Transform spawnPoint;
+
+    /*Direction:
+     * Place a collider in out of bound area
+     * Turn on isTrigger on collider
+     * Attach this script to GameObject
+     * Create another GameObject and specify it as respawn point
+     * Select the respawn point GameObject in the spawnPoint field 
+     */
+
+    [SerializeField] Transform spawnPoint; //Lets you enter spawnPoint from Unity
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")) //If Player enters Out Of Bounds
+        if (collision.gameObject.CompareTag("Player")) //If Player object enters Out Of Bounds
         {
-            collision.transform.position = spawnPoint.position; // Teleport from OutOfBounds to Respawn Point
+            collision.transform.position = spawnPoint.position; //Teleport from OutOfBounds area to spawnPoint coordinates
         }
     }
 }
