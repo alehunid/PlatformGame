ff75ed5dce1a446d1a481f2a1b6099a7772c3be3
johnw7@uci.edu <johnw7@uci.edu>
2018-11-24 03:29:17 -0800

    John - Added folder and animations
    - Added new folder to start project
    - Copied movement files into folder
    - Created animations (e.g. walk, jump)
    - Edited physics value to feel more like ghost

---
+++ VGDCPlatformer/Assets/_Main_/Scripts/Player/PlayerMoveMain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveMain : MonoBehaviour
+{
+
+    [SerializeField] private float runSpeed;
+    float horizontalMove = 0f;
+    bool jump = false;
+    public CharacterController2DMain controller;
+    public Animator animator;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", jump);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+        animator.SetBool("IsJumping", jump);
+    }
+
+}
